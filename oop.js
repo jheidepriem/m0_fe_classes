e134f34efe204a5979eefc13209f6fe17b144afb
Josephine Heidepriem <jheidepriem@gmail.com>
2022-07-17 23:32:59 -0500

    Add changes to classes documents

--- oop.js
+++ oop.js
@@ -2,6 +2,20 @@
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class unicorn {
+  constructor(name, color, say){
+  this.name = name;
+  this.color = "green";
+  this.say = say;
+}
+  changesay(newsay) {
+  this.say = newsay;
+  }
+
+}
+  var Unicorn1 = new unicorn("Uniballoony");
+  Unicorn1.changesay("*~*");
+    console.log(Unicorn1);
 
 
 
@@ -10,7 +24,23 @@
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
-
+class vampire {
+  constructor(name){
+  this.name = name
+  this.pet = "bat"
+  this.thirsty = true
+}
+  changethirsty(newthirsty) {
+      this.thirsty = newthirsty;
+    }
+    changepet(newpet){
+      this.pet = newpet;
+    }
+}
+  var vampire1 = new vampire("dracula");
+  vampire1.changethirsty(false);
+  vampire1.changepet("dog")
+  console.log(vampire1);
 
 
 //  Write a Dragon class
@@ -20,6 +50,26 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+
+class Dragon {
+  constructor(name, rider, color) {
+  this.name = name
+  this.rider = rider
+  this.color = color
+  this.eat = 1;
+  this.isHungry = true;
+  }
+  changeeat(neweat) {
+      this.eat = neweat;
+        if (this.eat >= 4) {
+        this.isHungry = false
+      }
+  }
+
+}
+  var Dragon1 = new Dragon("gary", "susan", "red" );
+  Dragon1.changeeat(8);
+  console.log(Dragon1);
 
 
 
@@ -31,3 +81,37 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+
+
+class Hobbit {
+  constructor(name, disposition) {
+    this.name = name;
+    this.disposition = disposition;
+    this.age = 0;
+    this.isAdult = false;
+    this.isOld = false;
+    this.hasRing = true;
+    this.birthday = 0;
+  }
+  changeage(newage) {
+      this.age = newage;
+        if (this.age >= 33) {
+        this.isAdult = true
+      }
+  }
+// Could not figure out how to add the celebrateBirthday method and change the
+// age by one. I'm pretty sure I would use the ++ increment, but I can't figure out
+//the proper syntax.
+  changename(newname) {
+      this.name = newname;
+        if (this.name !== "Frodo") {
+        this.hasRing = false
+      }
+  }
+//struggling to figure out how to get hasRing to print false with the name samuel
+}
+var Hobbit1 = new Hobbit("frodo", "cheerful");
+  Hobbit1.changeage(35);
+  Hobbit1.changename("samuel");
+    console.log(Hobbit1);
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+
+I had a hard time with the last challenge. I wasn't able to figure part of it out. I did try to google but it's just not making sense to me. I reached out to a few of my classmates to see if they could help explain that section to me.
+
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+
+Workflow has been good. I definitely need to take more breaks, because I can see how quickly work can become unproductive if I don't.
+
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+
+I need my kids to be in school and my house to be quiet so I can focus. I ordered new headphones and a new monitor and I'm changing my workspace. 
